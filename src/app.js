a8d3e70af117911be0152eca2652ab8006ccd54f
RakshithBhat03 <rakshith6666@gmail.com>
2021-08-22 10:14:58 +0530

    updated styles

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ const isTrinagleButton = document.querySelector(".isTriangleButton");
 const userOutput = document.querySelector("#userOutput");
 isTrinagleButton.addEventListener("click", () => {
   if (
-    Number(angleInput[0].value) +
-      Number(angleInput[1].value) +
-      Number(angleInput[2].value) ===
-    180
+    (angleInput[0].value !== "") &
+    (angleInput[1].value !== "") &
+    (angleInput[2].value !== "")
   ) {
-    userOutput.innerText = "It's a Triangle";
+    if (
+      Number(angleInput[0].value) +
+        Number(angleInput[1].value) +
+        Number(angleInput[2].value) ===
+      180
+    ) {
+      userOutput.innerText = "It's a Triangle";
+    } else {
+      userOutput.innerText = "Oops, it's not a Triangle";
+    }
   } else {
-    userOutput.innerText = "Oops,it's not a Triangle";
+    userOutput.innerText = "Enter all the fields";
   }
 });
